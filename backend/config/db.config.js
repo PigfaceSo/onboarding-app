845b3c5f87470b86554a572702e2a99f7e248f6d
andreifiatIG <andrei@inspiringgroup.sg>
2025-06-05 15:30:09 +0700

    Add Vercel routing configuration

--- backend/config/db.config.js
+++ backend/config/db.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   HOST: "localhost",
-  USER: "postgres",      // Replace with your PostgreSQL username
-  PASSWORD: "1806",  // Replace with your PostgreSQL password
-  DB: "villa_onboarding",         // Replace with your PostgreSQL database name
+  USER: "postgres",      
+  PASSWORD: "1806",  
+  DB: "villa_onboarding",        
   dialect: "postgres",
   pool: {
-    max: 5,       // max number of connections in pool
-    min: 0,       // min number of connections in pool
-    acquire: 30000, // maximum time, in milliseconds, that pool will try to get connection before throwing error
-    idle: 10000     // maximum time, in milliseconds, that a connection can be idle before being released
+    max: 5,      
+    min: 0,       
+    acquire: 30000, 
+    idle: 10000    
   }
 };
